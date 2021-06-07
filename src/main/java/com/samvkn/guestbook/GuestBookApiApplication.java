002cd62447baec8fdd4e2514328c6c0da4c77040
Samuel Clinton <contato@samvkn.dev>
2021-06-07 02:50:58 -0300

    Project created and configured

---
+++ src/main/java/com/samvkn/guestbook/GuestBookApiApplication.java
@@ -0,0 +1,13 @@
+package com.samvkn.guestbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBookApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestBookApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/samvkn/guestbook/GuestBookApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samvkn.guestbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBookApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
