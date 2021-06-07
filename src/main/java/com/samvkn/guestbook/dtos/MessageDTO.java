74623e5261ee40b8ddc822ca90cae039bf452963
Samuel Clinton <contato@samvkn.dev>
2021-06-07 02:55:14 -0300

    Created the Message data transfer object

---
+++ src/main/java/com/samvkn/guestbook/dtos/MessageDTO.java
@@ -0,0 +1,53 @@
+package com.samvkn.guestbook.dtos;
+
+import com.samvkn.guestbook.entities.Message;
+
+import java.io.Serializable;
+
+public class MessageDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+    private String message;
+    private String signature;
+
+    public MessageDTO() {
+    }
+
+    public MessageDTO(Long id, String message, String signature) {
+        this.id = id;
+        this.message = message;
+        this.signature = signature;
+    }
+
+    public MessageDTO(Message entity) {
+        id = entity.getId();
+        message = entity.getMessage();
+        signature = entity.getSignature();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public void setSignature(String signature) {
+        this.signature = signature;
+    }
+}
