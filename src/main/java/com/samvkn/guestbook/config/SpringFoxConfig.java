4396ee2dffb2cf6bebc5102626b3bb377b6adee3
Samuel Clinton <contato@samvkn.dev>
2021-06-24 17:08:20 -0300

    Finished implementing API documentation with Swagger

--- src/main/java/com/samvkn/guestbook/config/SpringFoxConfig.java
+++ src/main/java/com/samvkn/guestbook/config/SpringFoxConfig.java
@@ -4,8 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+
+import java.util.Collections;
 
 @Configuration
 public class SpringFoxConfig {
@@ -14,8 +18,13 @@ public class SpringFoxConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                     .select()
-                    .apis(RequestHandlerSelectors.any())
+                    .apis(RequestHandlerSelectors.basePackage("com.samvkn"))
                     .paths(PathSelectors.any())
-                    .build();
+                    .build()
+                    .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("Guestbook API", "Simple API for saving messages left by visitors using in a JSON format.", "1.0.0", "/tos", new Contact("Samuel Clinton", "https://samvkn.dev", "contato@samvkn.dev"), "Apache License", "/license", Collections.emptyList());
     }
 }
